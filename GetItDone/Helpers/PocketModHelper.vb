bedc11daf23dd69973552e90ab3012ed271e597e
alfar <arne@whee.dk>
2012-05-28 22:17:34 +0200

    Changed to use MigraDoc instead of PdfSharp pure

--- GetItDone/Helpers/PocketModHelper.vb
+++ GetItDone/Helpers/PocketModHelper.vb
@@ -5,8 +5,9 @@ Public Class PocketModHelper
     Private _Document As New PdfDocument()
     Private _Page As PdfPage
     Private _gfx As XGraphics
-    Private titlefont As XFont = New XFont("Verdana", 16, XFontStyle.Bold)
-    Private textfont As XFont = New XFont("Verdana", 10, XFontStyle.Regular)
+    Private options As New XPdfFontOptions(PdfFontEncoding.Unicode, PdfFontEmbedding.Always)
+    Private titlefont As XFont = New XFont("Verdana", 11.3, XFontStyle.Bold, options)
+    Private textfont As XFont = New XFont("Verdana", 10, XFontStyle.Regular, options)
     Private _originalState As XGraphicsState
 
     Public Sub New()
@@ -35,49 +36,104 @@ Public Class PocketModHelper
     End Sub
 
     Public Sub DrawFrontPage(page As Integer)
-        DrawText(page, "GetToDone.dk", True, True, PageHeight / 2 - 10)
+        GotoPage(page)
 
-        DrawText(page, "This pocketMod belongs to", False, True, PageHeight / 2 + 15)
-        DrawText(page, Membership.GetUser().UserName, False, True, PageHeight / 2 + 34)
+        Dim mDoc As New MigraDoc.DocumentObjectModel.Document()
+        Dim mSec As MigraDoc.DocumentObjectModel.Section = mDoc.AddSection()
+        mSec.PageSetup.TopMargin = MigraDoc.DocumentObjectModel.Unit.FromCentimeter(7)
+        Dim mPar As MigraDoc.DocumentObjectModel.Paragraph = mSec.AddParagraph("GetToDone.dk")
+        mPar.Format.Alignment = MigraDoc.DocumentObjectModel.ParagraphAlignment.Center
+        mPar.Format.Font.Name = "Verdana"
+        mPar.Format.Font.Size = 48
+        mPar.Format.Font.Bold = True
+        mPar.Format.Borders.Distance = "1cm"
+
+        Dim tPar As MigraDoc.DocumentObjectModel.Paragraph = mSec.AddParagraph()
+        tPar.Format.Alignment = MigraDoc.DocumentObjectModel.ParagraphAlignment.Center
+        tPar.Format.Font.Name = "Verdana"
+        tPar.Format.Font.Size = 24
+        tPar.Format.Borders.Distance = "1cm"
+        tPar.AddText("This pocketMod belongs to")
+        tPar.AddLineBreak()
+        tPar.AddLineBreak()
+        tPar.AddText(Membership.GetUser().UserName)
+        tPar.AddLineBreak()
+        tPar.AddLineBreak()
+        tPar.AddText(Membership.GetUser().Email)
+
+        Dim docRender As New MigraDoc.Rendering.DocumentRenderer(mDoc)
+        docRender.PrepareDocument()
+
+        Dim container As XGraphicsContainer = _gfx.BeginContainer(New XRect(Pages(page).Corner, New XSize(PageWidth, PageHeight)), New XRect(0, 0, XUnit.FromCentimeter(21), XUnit.FromCentimeter(29.7)), XGraphicsUnit.Point)
+        If Pages(page).Flipped Then
+            _gfx.RotateTransform(180)
+        End If
+        docRender.RenderPage(_gfx, 1)
+        _gfx.EndContainer(container)
     End Sub
 
     Public Sub DrawTasks(page As Integer, title As String, tasks As IEnumerable(Of TaskListModel))
+        Dim mDoc As New MigraDoc.DocumentObjectModel.Document()
+        Dim mSec As MigraDoc.DocumentObjectModel.Section = mDoc.AddSection()
+        mSec.PageSetup.TopMargin = MigraDoc.DocumentObjectModel.Unit.FromCentimeter(1)
+
         Dim offset As Integer = 10
         If Not String.IsNullOrEmpty(title) Then
-            DrawText(page, title, True, False, 10)
-            _gfx.DrawLine(XPens.Black, 5, 30, PageWidth - 10, 30)
-            offset = 30
+            Dim mPar As MigraDoc.DocumentObjectModel.Paragraph = mSec.AddParagraph(title)
+            mPar.Format.Alignment = MigraDoc.DocumentObjectModel.ParagraphAlignment.Center
+            mPar.Format.Font.Name = "Verdana"
+            mPar.Format.Font.Size = 32
+            mPar.Format.Font.Bold = True
+            mPar.Format.Borders.Distance = "5pt"
         End If
 
         For Each t As TaskListModel In tasks
-            DrawCheckedText(page, t.Title, offset)
-            offset += 12
+            Dim tPar As MigraDoc.DocumentObjectModel.Paragraph = mSec.AddParagraph()
+            tPar.Format.Alignment = MigraDoc.DocumentObjectModel.ParagraphAlignment.Left
+            tPar.Format.Font.Name = "Verdana"
+            tPar.Format.Font.Size = 24
+            tPar.Format.Borders.Distance = "5pt"
+            tPar.Format.LeftIndent = MigraDoc.DocumentObjectModel.Unit.FromCentimeter(1.2)
+            tPar.Format.FirstLineIndent = MigraDoc.DocumentObjectModel.Unit.FromCentimeter(-1.2)
+            Dim img As MigraDoc.DocumentObjectModel.Shapes.Image = tPar.AddImage(HttpContext.Current.Server.MapPath("/content/themes/base/images/pdfcheckbox.pdf"))
+            img.RelativeVertical = MigraDoc.DocumentObjectModel.Shapes.RelativeVertical.Line
+            img.Height = MigraDoc.DocumentObjectModel.Unit.FromMillimeter(6)
+            img.Width = MigraDoc.DocumentObjectModel.Unit.FromMillimeter(6)
+            tPar.AddTab()
+            tPar.AddText(t.Title)
         Next
+
+        Dim docRender As New MigraDoc.Rendering.DocumentRenderer(mDoc)
+        docRender.PrepareDocument()
+
+        Dim container As XGraphicsContainer = _gfx.BeginContainer(New XRect(Pages(page).Corner, New XSize(PageWidth, PageHeight)), New XRect(0, 0, XUnit.FromCentimeter(21), XUnit.FromCentimeter(29.7)), XGraphicsUnit.Point)
+        If Pages(page).Flipped Then
+            _gfx.RotateTransform(180)
+        End If
+        docRender.RenderPage(_gfx, 1)
+        _gfx.EndContainer(container)
     End Sub
 
     Public Sub DrawInbox(page As Integer)
         DrawText(page, "Inbox", True, False, 10)
 
-        For offset As Integer = 30 To PageHeight Step 20
+        For offset As Integer = 25 To PageHeight Step 20
             _gfx.DrawLine(XPens.Black, 5, offset, PageWidth - 10, offset)
         Next
     End Sub
 
-    Public Sub DrawCheckedText(page As Integer, text As String, offsetY As Integer)
-        GotoPage(page)
-
-
-        _gfx.DrawRectangle(XPens.Black, 5, offsetY + 2, 10, 10)
-        _gfx.DrawString(text, textfont, XBrushes.Black, New XRect(20, offsetY, PageWidth - 25, 12), XStringFormats.TopLeft)
-    End Sub
-
-
     Public Sub DrawText(page As Integer, text As String, title As Boolean, center As Boolean, offsetY As Integer)
         GotoPage(page)
         If title Then
+            _gfx.Save()
+            _gfx.IntersectClip(New XRect(5, offsetY, PageWidth - 10, 20))
             _gfx.DrawString(text, titlefont, XBrushes.Black, New XRect(5, offsetY, PageWidth - 10, 20), If(center, XStringFormats.Center, XStringFormats.TopLeft))
+            _gfx.Restore()
         Else
+            _gfx.Save()
+            _gfx.IntersectClip(New XRect(5, offsetY, PageWidth - 10, 12))
             _gfx.DrawString(text, textfont, XBrushes.Black, New XRect(5, offsetY, PageWidth - 10, 12), If(center, XStringFormats.Center, XStringFormats.TopLeft))
+            _gfx.Restore()
         End If
     End Sub
 
